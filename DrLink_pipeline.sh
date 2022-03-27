380f9027f8f2d76a2b5b4476b3f4e359aed930ad
HeqSun <sunhequan@gmail.com>
2022-03-27 22:58:04 +0200

    Update DrLink_pipeline.sh

--- DrLink_pipeline.sh
+++ DrLink_pipeline.sh
@@ -1,7 +1,5 @@
 ###################################  DrLink pipeline (documentation)  ##################################################
 ######################  under development by Hequan Sun              (MPIPZ, Germany)  #################################
-######################                       Beth A. Rowan           (UC Davis, USA)   #################################
-######################                       Korbinian Schneeberger  (MPIPZ, Germany)  #################################
 ####       given longranger read alignments (molecule recovery), read-barcodes in vcf (barcode extraction) #############
 ####       and parental SNP markers, DrLink predicts CO breakpoints                                    #################
 #
